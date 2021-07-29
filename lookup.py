5658825239c2fe2e1d2b450d1d962597d2214e63
0stucker <49743311+0stucker@users.noreply.github.com>
2021-07-29 15:00:08 -0500

    Update lookup.py
    added gui through pysimplegui
    option to refresh without closing program

--- lookup.py
+++ lookup.py
@@ -1,3 +1,4 @@
+import PySimpleGUI as sg
 import spotipy
 import string
 import spotipy.oauth2 as oauth2
@@ -8,26 +9,57 @@ from spotipy.oauth2 import SpotifyOAuth
 
 scope = "user-library-read,user-read-currently-playing,user-read-playback-state,user-read-private"
 client_id = ''
-client_secret = ''
-redirect_uri = ''
+client_secret = '' #py2exe credentials?
+redirect_uri = 'http://localhost:8080'
 bannedsymbols = '!,.@&)('
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth( client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
-yo2 = sp.current_playback()
 
-songdump = json.dumps(yo2,indent=4)
-data = json.loads(songdump)
+def open_lyrics(bandname,songname): #genius api later? az missing tracks
+    webbrowser.open_new("https://www.azlyrics.com/lyrics/" + bandname + "/" + songname + ".html") 
 
-for i in data['item']['album']['artists']:
-   bandname= i['name']
+def songrefresh():
+    currentsong = sp.current_playback()
+    songdump = json.dumps(currentsong,indent=4)
+    data = json.loads(songdump)
+    for i in data['item']['album']['artists']:
+         bandname= i['name']
+    songname = data['item']['name']
+    songname = songname.replace(" ", "")
+    bandname = bandname.replace(" ", "")
+    bandname = bandname.replace(bannedsymbols, "") #less lines 
+    songname = songname.lower()
+    bandname = bandname.lower()
+    for c in songname:
+         for c in bannedsymbols:
+            songname = songname.replace(c, "")
+    return [bandname, songname]
+        
+    
 
-songname = data['item']['name']
-songname = songname.replace(" ", "")
-bandname = bandname.replace(" ", "")
-songname = songname.lower()
-bandname = bandname.lower()
-for c in songname:
-    for c in bannedsymbols:
-        songname = songname.replace(c, "")
+bandname = '                                         ' #text output matches this length
+songname = '                                         '
+layout = [[sg.Text(bandname, key ='-BN-')], [sg.Text(songname, key='-SN-')],
+          [sg.Text(size=(40,1), key='-OUTPUT-')],
+          [sg.Button('Open Lyrics'), sg.Button('Refresh'), sg.Button('Quit')]]
 
-webbrowser.open_new("https://www.azlyrics.com/lyrics/" + bandname + "/" + songname + ".html")
+# Create the window
+window = sg.Window('Yo', layout)
+
+# Display and interact with the Window using an Event Loop
+while True:
+    event, values = window.read()
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    elif event == 'Open Lyrics':
+        open_lyrics(bandname,songname)
+    elif event == 'Refresh':
+        infolist = songrefresh()
+        bandname = infolist[0]
+        songname = infolist[1]
+        window['-BN-'](bandname)
+        window['-SN-'](songname)
+        print(bandname)
+
+
+window.close()
